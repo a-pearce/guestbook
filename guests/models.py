66ba8ae6d5f70139041de61b73d43b1c6f143a1d
Anthony Pearce <a-pearce@sbcglobal.net>
2014-09-13 23:35:17 -0500

    add entry class

--- guests/models.py
+++ guests/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    name = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    message = models.TextField(blank=True)
